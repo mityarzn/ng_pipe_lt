4f12c1b788670c61514a90729ac509428d51d6dd
Dmitry Petuhov <mityapetuhov@gmail.com>
2015-03-03 22:30:10 +0300

    Fix is_p2p 27000-27999 port range

--- ng_pipe_lt.c
+++ ng_pipe_lt.c
@@ -445,7 +445,7 @@ try_pullup(struct mbuf **m, int len)
 
 /* Function that tries to identify p2p packets by TCP/UDP ports */
 static inline int
-is_p2p(register uint16_t port, uint16_t proto)
+ (register uint16_t port, uint16_t proto)
 {
 	return (port > 6881 && port < 6999) || // Torrent ports
 	(port > 10000 && ( // High-order ports are often client-side, except some
@@ -454,7 +454,7 @@ is_p2p(register uint16_t port, uint16_t proto)
 	 port != 26901)
 	||
 	(proto == IPPROTO_UDP && port != 20560 && port != 26000 && port != 28960 &&
-	 port != 29000 && (port < 27000 || port < 27999))));
+	 port != 29000 && port < 27000 && port > 27999))));
 }
 /*
  * Check if packet is TCP ACK. Here we expecting not-vlan-tagged Ethernet frames with IP
